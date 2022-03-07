772721532dcad6010e7ce979c7269bd73b12054f
Helena Paiva <hellenapaiva@msn.com>
2022-03-07 01:58:54 -0300

    template animation com framer-motion

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import GlobalStyles from './styles/global';
+import { Main, Section, Sticky } from './styles';
+import FirstAndSecond from './components/FirstAndSecond';
+import { useEffect } from 'react';
+import Header from './components/Header';
+
+function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+
+      <Header />
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
+  );
+}
+
+export default App;
